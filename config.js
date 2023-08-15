8252b0dea3e218d7e5ad9d31dd46956f7b1db76d
epic-person-on <123142178+epic-person-on@users.noreply.github.com>
2023-08-15 10:30:01 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+
+const config = {
+  BOOGLE_API_KEY: 'AIzaSyDDynEjIwNZ4smHniiBoWrlx6EIE0gxvaI',
+  BOUTUBE_API_KEY: 'AIzaSyCmLW1y_He5wmvH3dMt1B-LW_Ox6DoLhb8',
+  BOOGLE_CX: 'a58e5a5b7b74f492b',
+};
+
+module.exports = config;
